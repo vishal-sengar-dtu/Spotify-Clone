de76c8a7e5439818227cdbca200739fd40c203ce
Vishal Sengar <vishalsengardtu@gmail.com>
2020-10-22 13:14:17 +0530

    Pull Token from URL

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Login from './Login';
+import { getTokenFromUrl } from "./spotify";
 import './App.css';
 
 function App() {
+  useEffect(() => {
+    const token = getTokenFromUrl();
+    console.log("I HAVE A TOKEN 👉 ", token)
+  }, []);
+
   return (
     <div className="app">
       <Login />
