e3e95e29a2d97c786d63269c694f053631516c65
Vishal Sengar <vishalsengardtu@gmail.com>
2020-10-23 22:24:18 +0530

    Sidebar Design

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import "./Sidebar.css";
 import SidebarOption from './SidebarOption';
+import HomeIcon from '@material-ui/icons/Home';
+import SearchIcon from '@material-ui/icons/Search';
+import LibraryMusicIcon from '@material-ui/icons/LibraryMusic';
 
 function Sidebar() {
   return (
@@ -10,9 +13,12 @@ function Sidebar() {
         src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg"
         alt="logo" 
       />
-      <SidebarOption option="Home" />
-      <SidebarOption option="Search" />
-      <SidebarOption option="Your Library"/>
+      <SidebarOption Icon={HomeIcon} title="Home" />
+      <SidebarOption Icon={SearchIcon} title="Search" />
+      <SidebarOption Icon={LibraryMusicIcon} title="Your Library" />
+      <br />
+      <strong className="sidebar_title">PLAYLISTS</strong>
+      <hr />
     </div>
   )
 }
--- src/SidebarOption.js
+++ src/SidebarOption.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './SidebarOption.css';
 
-function SidebarOption() {
+function SidebarOption({title, Icon}) {
   return (
     <div className="sidebarOption">
-      
+      {Icon && <Icon className="sidebarOption_icon" />}
+      {Icon ? <h4>{title}</h4> : <p>{title}</p>}
     </div>
   )
 }
